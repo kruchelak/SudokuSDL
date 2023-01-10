7cb5b192ecfaecbbd85429453fe56d073d5e2562
kruchelak <domin.kruchelski@gmail.com>
2023-01-10 16:28:18 +0100

    Added input handler

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 #include <stdbool.h>
 #include <SDL2_gfxPrimitives.h>
 #include <limits.h>
+#include <errno.h>
+
 #define MAX_SURFACES_X 9
 #define MAX_SURFACES_Y 9
 #define WINDOW_WIDTH 450
@@ -12,20 +14,46 @@
 #define ENOUGH snprintf(NULL, 0,"%d",42)
 
 
-//to bedzie wczytywae z pliku
-int numbers[3][9] = {
-        {1, 0, 3, 4, 5, 6, 7, 8, 9},
-        {4, 5, 6, 7, 0, 9, 1, 2, 3},
-        {7, 8, 9, 1, 2, 3, 4, 5, 6}
-};
+SDL_Rect rects[9][9];
+int clickedRect[9][9];
 
+
+void getNumbers(int level, int startNumbers[9][9]){
+    //stworzenie sciezki do pliku, bufor 25 poniewaz taka jest dlugosc stringa + znak \0
+    printf("%d\n", level);
+//    char path[29];
+//    snprintf(path, 29, "./Levels/Level_%d/start_%d.txt", poziom, poziom);
+//    printf("%s\n", path); // dziala
+    FILE *file = fopen("/Users/kundelski/CLionProjects/SudokuSDL/assets/levels/start1.txt","r");
+    if (file == NULL) {
+        // wystąpił błąd podczas otwierania pliku
+        fprintf(stderr, "Nie udało się otworzyć pliku: %s\n", strerror(errno));
+        return;
+    }
+    //wkladanie liczb z pliku do tablicy
+    for(int i = 0; i<9; i++){
+        for(int j = 0; j<9; j++){
+            fscanf(file, "%d", &startNumbers[j][i]);
+        }
+    }
+    fclose(file);
+
+}
+
+void updateRect(void){
+    SDL_StopTextInput();
+
+
+}
 void checkClickInRects(int mouseX, int mouseY) {
     for(int i = 0; i < 9; i++) {
         for(int j = 0; j < 9; j++) {
-            SDL_Rect rect = {i * (WINDOW_WIDTH / 9), j * (WINDOW_HEIGHT / 9), 50, 50};
             SDL_Point mousePoint = {mouseX, mouseY};
-            if (SDL_PointInRect(&mousePoint, &rect)) {
+            if (SDL_PointInRect(&mousePoint, &rects[i][j])) {
                 printf("Kliknięcie nastąpiło w prostokącie (%d, %d)\n", i, j);
+                SDL_StartTextInput();
+
+
             }
         }
     }
@@ -35,7 +63,7 @@ void isMouseInRect(void) {
     SDL_GetMouseState(&mouseX,&mouseY);
     checkClickInRects(mouseX, mouseY);
 }
-void createRects(SDL_Renderer* renderer){
+void createRects(SDL_Renderer* renderer, int startNumbers[9][9]){
     // ustawianie fontu
     TTF_Font *font = TTF_OpenFont("./assets/pacifico/Pacifico.ttf", 24);
     if (!font) {
@@ -48,15 +76,15 @@ void createRects(SDL_Renderer* renderer){
 
     for(int i = 0; i<9 ; i++){
         for(int j = 0; j<9; j++) {
-            SDL_Rect rect = {i * (WINDOW_WIDTH / 9), j * (WINDOW_HEIGHT / 9), 50, 50};
+            rects[i][j] = (SDL_Rect){i * (WINDOW_WIDTH / 9), j * (WINDOW_HEIGHT / 9), 50, 50};
 
             SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
-            SDL_RenderDrawRect(renderer, &rect);
+            SDL_RenderDrawRect(renderer, &rects[i][j]);
 
             //przeksztalca cyfre w stringa,
             char number_str[ENOUGH+1];
-            sprintf(number_str, "%d", numbers[i][j]);
+            sprintf(number_str, "%d", startNumbers[i][j]);
 
 
 
@@ -70,8 +98,8 @@ void createRects(SDL_Renderer* renderer){
 
             // tworzenie tekstury z powierzchni, brak tekstury jesli pole = 0
             SDL_Texture *text_texture = SDL_CreateTextureFromSurface(renderer, textSurfaces[i][j]);
-            if (numbers[i][j] != 0) {
-                SDL_RenderCopy(renderer, text_texture, NULL, &rect);
+            if (startNumbers[i][j] != 0) {
+                SDL_RenderCopy(renderer, text_texture, NULL, &rects[i][j]);
             }
 
         }
@@ -94,6 +122,14 @@ void createLines( SDL_Renderer* renderer){
     }
 }
 int main(void) {
+    //wczytanie poziomu
+//    int level;
+//    scanf("%d", &level);
+    int startNumbers[9][9];
+    getNumbers(1, startNumbers);
+
+
+
     bool quit = false;
     SDL_Event event;
 
@@ -108,7 +144,7 @@ int main(void) {
     SDL_SetRenderDrawColor(renderer, 255,255,255,255);
     SDL_RenderClear(renderer);
     //rysowanie kwadratow
-    createRects(renderer);
+    createRects(renderer, startNumbers);
     // render linii
     createLines(renderer);
 
@@ -126,7 +162,11 @@ int main(void) {
                 break;
             case SDL_MOUSEBUTTONDOWN:
                 isMouseInRect();
-
+                break;
+            case SDL_TEXTINPUT:
+                printf("test");
+                updateRect();
+                break;
         }
     }
     SDL_DestroyRenderer(renderer);
