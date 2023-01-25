f8e357b409ca9354ab36c15a0e0ec3c94a1ad0f0
kruchelak <domin.kruchelski@gmail.com>
2023-01-25 12:20:13 +0100

    added 2 another levels

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <SDL.h>
 #include <SDL_ttf.h>
-#include <stdbool.h>
+#include <stdbool.h> //i know i shouldn't
 #include <SDL2_gfxPrimitives.h>
 #include <errno.h>
 
@@ -12,8 +12,8 @@
 #define WINDOW_HEIGHT 450
 //https://stackoverflow.com/questions/8257714/how-to-convert-an-int-to-string-in-c
 #define ENOUGH snprintf(NULL, 0,"%d",42)
-int startNumbers[9][9];
-int correctNumbers[9][9];
+int startNumbers[9][9]; //start board
+int correctNumbers[9][9]; //complete board
 
 typedef struct rect{
     bool isClicked;
@@ -24,24 +24,21 @@ rect rects[9][9];
 
 
 void get_numbers(int level){
-    //stworzenie sciezki do pliku, bufor 27 poniewaz taka jest dlugosc stringa + znak \0
+    //creates path to file, bufor equals 27 : string length + \0 sign
     printf("%d\n", level);
     char pathstart[27];
     char pathend[25];
     snprintf(pathstart, 27, "./assets/levels/start%d.txt", level);
-
     snprintf(pathend, 25, "./assets/levels/end%d.txt", level);
-    printf("%s\n", pathstart);
-    printf("%s\n", pathend);
 
     FILE *start = fopen(pathstart, "r");
     FILE *correct = fopen(pathend,"r");
     if (start == NULL || correct == NULL) {
-        // wystąpił błąd podczas otwierania pliku
-        fprintf(stderr, "Nie udało się otworzyć pliku: %s\n", strerror(errno));
+        //error while opening the file
+        fprintf(stderr, "Can't open the file %s\n", strerror(errno));
         return;
     }
-    //wkladanie liczb z pliku do tablicy
+    //puts numbers from files to arrays
     for(int i = 0; i<9; i++){
         for(int j = 0; j<9; j++){
             fscanf(start, "%d", &startNumbers[j][i]);
@@ -56,34 +53,34 @@ void get_numbers(int level){
 
 }
 int check_win(void){
+    //compares current state of board with correct board
     int a = memcmp(correctNumbers, startNumbers, sizeof(correctNumbers));
     if(!a)
-        return 0; // arrays are equal
+        return 0; //arrays are equal
     else
         return 1; //error, arrays are not equal
 }
 
 
 void create_rects(SDL_Renderer* renderer){
-    // ustawianie fontu
+    //sets font
     TTF_Font *font = TTF_OpenFont("./assets/OpenSans/OpenSans-Regular.ttf", 128);
     if (!font) {
-        fprintf(stderr, "Nie udało się otworzyć fontu: %s\n", TTF_GetError());
+        fprintf(stderr, "Can't open font file: %s\n", TTF_GetError());
         exit(1);
     }
-    //ustawianie koloru tekstu
+    //sets text color
     SDL_Color textColor = {0, 0, 0, 255};
     SDL_Surface* textSurfaces[MAX_SURFACES_X][MAX_SURFACES_Y];
 
     for(int i = 0; i<9 ; i++){
         for(int j = 0; j<9; j++) {
             rects[i][j].rect = (SDL_Rect) {i * (WINDOW_WIDTH / 9), j * (WINDOW_HEIGHT / 9), 50, 50};
-
-
+            //sets green background if rect is clicked
             if (rects[i][j].isClicked == true) {
             SDL_SetRenderDrawColor(renderer, 0, 150, 50, 10);
             SDL_RenderFillRect(renderer, &rects[i][j].rect);
-        }
+            }
             else {
                 SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
                 SDL_RenderDrawRect(renderer, &rects[i][j].rect);
@@ -91,24 +88,19 @@ void create_rects(SDL_Renderer* renderer){
 
             rects[i][j].isClicked = false;
 
-
-
-//            SDL_RenderDrawRect(renderer, &rects[i][j].rect);
-
-            //przeksztalca cyfre w stringa,
+            //number to string, needed to put it onto the surface
             char number_str[ENOUGH+1];
             sprintf(number_str, "%d", startNumbers[i][j]);
 
 
-            // tworzenie powierzchni rysowania dla tekstu
-
+            //creates a surface for text
             textSurfaces[i][j] = TTF_RenderText_Solid(font, number_str, textColor);
             if (!textSurfaces[i][j]) {
-                fprintf(stderr, "Nie udało się stworzyć powierzchni tekstu: %s\n", TTF_GetError());
+                fprintf(stderr, "Can't create text surface: %s\n", TTF_GetError());
                 exit(1);
             }
 
-            // tworzenie tekstury z powierzchni, brak tekstury jesli pole = 0
+            //creates texture from surface, no texture if number = 0
             SDL_Texture *text_texture = SDL_CreateTextureFromSurface(renderer, textSurfaces[i][j]);
             if (startNumbers[i][j] != 0) {
                 SDL_RenderCopy(renderer, text_texture, NULL, &rects[i][j].rect);
@@ -119,7 +111,7 @@ void create_rects(SDL_Renderer* renderer){
 
     }
     if(!check_win()){
-        printf("WYGRANA!");
+        printf("YOU WON!");
     }
 
     TTF_CloseFont(font);
@@ -129,6 +121,7 @@ void create_rects(SDL_Renderer* renderer){
         }
     }
 }
+//creates 2 vertical, 2 horizontal thick lines between 3x3 squares
 void create_lines(SDL_Renderer* renderer){
     for(int i =1 ;i<3 ;i++) {
         thickLineRGBA(renderer, (WINDOW_WIDTH/3)*i, 0, (WINDOW_WIDTH/3)*i, WINDOW_HEIGHT, 5, 0, 0, 0, 255);
@@ -136,32 +129,33 @@ void create_lines(SDL_Renderer* renderer){
 
     }
 }
+//shows instructions, waits for user input
 int pick_level(void){
 
     int level=0;
 
-    printf("Wybierz poziom 1-3\n");
+    printf("Enter level difficulty\nEasy - 1\nMedium - 2\nHard - 3\n");
     while(level <1 || level >3){
 
         scanf("%d", &level);
         if(level <1 || level >3)
-         printf("Zły znak.Wybierz liczbe od 1 do 3.\n");
+         printf("Wrong input. Pick a number between 1 and 3\n");
 
     }
 
 
     return level;
 }
+//creates new screen everytime this function is called
 void update_screen(SDL_Renderer* renderer){
     SDL_SetRenderDrawColor(renderer, 255,255,255,255);
     SDL_RenderClear(renderer);
-    //rysowanie kwadratow
     create_rects(renderer);
-    // render linii
     create_lines(renderer);
 
     SDL_RenderPresent(renderer);
 }
+//checks in which rect click occurred
 void check_mouse_in_rect(SDL_Renderer* renderer) {
     int mouseX, mouseY;
     SDL_GetMouseState(&mouseX,&mouseY);
@@ -169,14 +163,11 @@ void check_mouse_in_rect(SDL_Renderer* renderer) {
         for(int j = 0; j < 9; j++) {
             SDL_Point mousePoint = {mouseX, mouseY};
             if (SDL_PointInRect(&mousePoint, &rects[i][j].rect)) {
-                printf("Kliknięcie nastąpiło w prostokącie (%d, %d)\n", i, j);
-
                 if(startNumbers[i][j] == 0) {
                     rects[i][j].isClicked = true;
                     SDL_StartTextInput();
                 }
                 else {
-                    printf("kliknieto zly prostokat\n");
                     update_screen(renderer);
                 }
 
@@ -187,16 +178,12 @@ void check_mouse_in_rect(SDL_Renderer* renderer) {
 
 }
 
-//x,y recta w tablicy rectow,
-// prawdopodobnie nadal lepiej byloby przekazywac tego recta przez funckje znajdujaca nacisnietego,
-// trzeba pomyslec czy sie oplaca to zmieniac
-//
+
 int x=0;
 int y=0;
-void updateRect(SDL_Renderer* renderer, char *string, int option){
-
+void update_rect(SDL_Renderer* renderer, char *string, int option){
+//option 1 is a mouse click
     if(option == 1){
-//        printf("kliknieto\n");
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
                 if(rects[i][j].isClicked == true){
@@ -209,17 +196,16 @@ void updateRect(SDL_Renderer* renderer, char *string, int option){
             }
         }
     }
+//option 2 is a text input
     if(option == 2){
         SDL_StopTextInput();
-//        printf("%d",atoi(string));
-//        printf("%d", correctNumbers[x][y]);
         if(atoi(string) == correctNumbers[x][y] && startNumbers[x][y] != correctNumbers[x][y]){
             startNumbers[x][y] =atoi(string);
-            printf("wpisana dobra liczbe\n\n");
+            printf("Correct Number\n\n");
             check_win();
         }
         else
-            printf("wpisano zla liczbe\n");
+            printf("Wrong input\n");
 
         update_screen(renderer);
 
@@ -234,12 +220,12 @@ int main(void) {
 
 
 
-    //wczytanie poziomu
-
+    //picking level,passes level number to get_numbers function
     get_numbers(pick_level());
 
     bool quit = false;
     SDL_Event event;
+    //initialises SDL, creates windows and renderer for sdl, bla bla bla
 
     SDL_Init(SDL_INIT_VIDEO);
     TTF_Init();
@@ -248,14 +234,11 @@ int main(void) {
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    //ustawianie koloru tla
-
-
     update_screen(renderer);
 
 
 
-    //czyszczenie pamieci
+    //event handling
     while (!quit) {
         SDL_WaitEvent(&event);
 
@@ -264,17 +247,17 @@ int main(void) {
                 quit = true;
                 break;
             case SDL_MOUSEBUTTONDOWN:
-//                check_win();
                 check_mouse_in_rect(renderer);
-                updateRect(renderer,"", 1);
+                update_rect(renderer, "", 1);
                 break;
             case SDL_TEXTINPUT:
 
 
-                updateRect(renderer, event.text.text, 2);
+                update_rect(renderer, event.text.text, 2);
                 break;
         }
     }
+    //cleansing memory
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
